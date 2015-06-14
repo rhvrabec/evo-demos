ef1e0794b89b1c383fa9f0f0714fc5ef2038bf1d
Martin Elwin <martin@elwin.com>
2015-06-14 18:19:46 +0200

    Add Lambda FunctionName AWS config parameter.

--- Demos2015/aws-iot-demo/aws-config.js
+++ Demos2015/aws-iot-demo/aws-config.js
@@ -1,8 +1,9 @@
 evothings.aws = evothings.aws || {}
 
-// TODO: Fill in your AWS keys here.
+// TODO: Fill in your AWS information here.
 evothings.aws.config = {
 	accessKeyId: 'YOUR_ACCESS_KEY_ID',
 	secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
-	region: 'YOUR_REGION'
+	region: 'YOUR_REGION',
+	params: { FunctionName: 'YOUR_LAMBDA_FUNCTION_NAME' }
 }
